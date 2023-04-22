fb297ef7ca081547a13170d4f6d082b6a0fe83ee
mrrolbot23 <roldandelgado@icloud.com>
2023-04-22 14:09:37 -0400

    initial commit

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Navigation.css";
+
+const Navigation = () => {
+  return (
+    <>
+      <nav className="navigation">
+        <button>
+          <a href="null">Sign Out</a>
+        </button>
+      </nav>
+    </>
+  );
+};
+
+export default Navigation;
