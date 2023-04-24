95b7310778195edbdff85ddf6de7b25983b18380
mrrolbot23 <roldandelgado@icloud.com>
2023-04-24 18:38:39 -0400

    no changes

--- src/App.js
+++ src/App.js
@@ -21,12 +21,11 @@ class App extends Component {
     };
   }
 
-  calculateFaceLocation = (data) => {
-    const face = data.outputs[0].data.regions[0].region_info.bounding_box;
+  calculateFaceLocation = (dataArray) => {
+    const face = dataArray.outputs[0].data.regions[0].region_info.bounding_box;
     const image = document.getElementById("input-image");
     const width = Number(image.width);
     const height = Number(image.height);
-    console.log(data);
     return {
       leftCol: face.left_col * width,
       topRow: face.top_row * height,
